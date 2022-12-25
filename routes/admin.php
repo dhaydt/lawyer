82e94b6ead56811fa703979938206b777474a42b
dhaydt <1992dayat@gmail.com>
2022-12-25 12:57:38 +0700

    web admin

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth.user')->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    // Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
     // USER ROUTE
     Route::get('list_admin', [UserController::class, 'index'])->name('admin.list');

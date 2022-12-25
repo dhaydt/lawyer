b76913db25cf33c6345bd4b23ba3f89b9a864ec0
dhaydt <1992dayat@gmail.com>
2022-12-25 14:30:10 +0700

    fix prefix admin

--- routes/admin.php
+++ routes/admin.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth.user')->group(function () {
-    // Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::middleware('auth.user')->prefix('admin')->as('admin.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
     // USER ROUTE
     Route::get('list_admin', [UserController::class, 'index'])->name('admin.list');

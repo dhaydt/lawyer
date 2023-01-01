deccff602dfb056a61889c593de8f853211528a9
dhaydt <1992dayat@gmail.com>
2023-01-01 08:55:02 +0700

    hide metronic img

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -55,10 +55,10 @@
             <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2"
                 style="background-image: url({{ ('assets_metronic/media/misc/auth-bg.png') }})">
                 <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
-                    <a href="javascript:" class="mb-0 mb-lg-12">
+                    {{-- <a href="javascript:" class="mb-0 mb-lg-12">
                         <img alt="Logo" src="{{ asset('assets_metronic/media/logos/custom-1.png') }}" class="h-60px h-lg-75px" />
-                    </a>
-                    <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20"
+                    </a> --}}
+                    {{-- <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20"
                         src="{{ asset('assets_metronic/media/misc/auth-screens.png') }}" alt="" />
                     <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">Fast, Efficient and
                         Productive</h1>
@@ -68,7 +68,7 @@
                         <br />and provides some background information about
                         <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the interviewee</a>and their
                         <br />work following this is a transcript of the interview.
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>

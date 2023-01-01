2f3503d54c1f129aaac88c50c3ffb06650e81407
dhaydt <1992dayat@gmail.com>
2023-01-01 08:58:03 +0700

    fix logo auth admin

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -52,12 +52,13 @@
                     </div>
                 </div>
             </div>
+            @php($logo= \App\CPU\Helpers::get_config('web_logo'))
             <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2"
                 style="background-image: url({{ ('assets_metronic/media/misc/auth-bg.png') }})">
                 <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
-                    {{-- <a href="javascript:" class="mb-0 mb-lg-12">
-                        <img alt="Logo" src="{{ asset('assets_metronic/media/logos/custom-1.png') }}" class="h-60px h-lg-75px" />
-                    </a> --}}
+                    <a href="javascript:" class="mb-0 mb-lg-12">
+                        <img alt="Logo" src="{{ asset('storage/company'.'/'.$logo) }}" class="h-60px h-lg-75px" />
+                    </a>
                     {{-- <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20"
                         src="{{ asset('assets_metronic/media/misc/auth-screens.png') }}" alt="" />
                     <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">Fast, Efficient and

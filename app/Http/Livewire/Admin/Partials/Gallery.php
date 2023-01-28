15dc1796a551be732447c184f96322d2250f365e
dhaydt <1992dayat@gmail.com>
2023-01-28 16:25:03 +0700

    fix dashboard 50%

--- app/Http/Livewire/Admin/Partials/Gallery.php
+++ app/Http/Livewire/Admin/Partials/Gallery.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire\Admin\Partials;
 
+use App\Models\Gallery as ModelsGallery;
 use Livewire\Component;
 
 class Gallery extends Component
 {
+    protected $gallery;
+
     public function render()
     {
-        return view('livewire.admin.partials.gallery');
+        $this->gallery = ModelsGallery::where('type', 'image')->orderBy('updated_at', 'desc')->get()->take(6);
+        $data['gallery'] = $this->gallery;
+        return view('livewire.admin.partials.gallery', $data);
     }
 }
--- app/Http/Livewire/Admin/Partials/Services.php
+++ app/Http/Livewire/Admin/Partials/Services.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Livewire\Admin\Partials;
 
+use App\Models\Services as ModelsServices;
 use Livewire\Component;
 
 class Services extends Component
 {
+    protected $services;
+
     public function render()
     {
-        return view('livewire.admin.partials.services');
+        $this->services = ModelsServices::orderBy('updated_at', 'desc')->get()->take(6);
+        $data['services'] = $this->services;
+
+        return view('livewire.admin.partials.services', $data);
     }
 }

3d5b8d9fcd8a1a01128d8c9d0f0d00c76c86e54e
dhaydt <1992dayat@gmail.com>
2023-02-21 11:18:02 +0700

    fix meta keyword

--- resources/views/Home/components/banner.blade.php
+++ resources/views/Home/components/banner.blade.php
@@ -1,7 +1,7 @@
 @push('css')
 <meta name="title" content="amar advokat website hukum advokat" />
 <meta name="description" content="Hukum Perusahaan - Hak Kekayaan Intelektual - Investasi - Properti - Pertambangan & Energi - Telekomunikasi - Dokumentasi Notaris - Perbankan & Keuangan - Asuransi - Ketenagakerjaan - Hukum Keluarga dan Perkawinan - Hukum Perdata & Pidana - Pengacara Retainer" />
-<meta name="keyword" content="Hukum-Perusahaan,Hak-Kekayaan-Intelektual,Investasi,Properti,Pertambangan-dan-Energi,Telekomunikasi,Dokumentasi-Notaris,Perbankan-dan-Keuangan,Asuransi,Ketenagakerjaan,Hukum-Keluarga-dan-Perkawinan,Hukum-Perdata-dan-Pidana,Pengacara-Retainer" />
+<meta name="keyword" content="amar-advokat,Hukum-Perusahaan,Hak-Kekayaan-Intelektual,Investasi,Properti,Pertambangan-dan-Energi,Telekomunikasi,Dokumentasi-Notaris,Perbankan-dan-Keuangan,Asuransi,Ketenagakerjaan,Hukum-Keluarga-dan-Perkawinan,Hukum-Perdata-dan-Pidana,Pengacara-Retainer" />
 <link rel="canonical" href="{{ route('home') }}">
 <meta property="og:image" content="{{ asset('storage/company'.'/'.$web_config['web_logo']) }}" />
 <meta property="og:title" content="Amar Advokat Company Profile Firma Hukum" />

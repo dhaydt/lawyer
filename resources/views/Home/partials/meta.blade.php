0e173b304663c3c36b83e91b6831fd14a664ccf0
dhaydt <1992dayat@gmail.com>
2023-02-27 14:39:56 +0700

    meta1

---
+++ resources/views/Home/partials/meta.blade.php
@@ -0,0 +1,13 @@
+<meta name="title" content="Amar Advokat || Website Hukum Advokat" />
+<meta name="description" content="amar advokat ,Hukum Perusahaan - Hak Kekayaan Intelektual - Investasi - Properti - Pertambangan & Energi - Telekomunikasi - Dokumentasi Notaris - Perbankan & Keuangan - Asuransi - Ketenagakerjaan - Hukum Keluarga dan Perkawinan - Hukum Perdata & Pidana - Pengacara Retainer" />
+<meta name="keyword" content="amar advokat amaradvokat Hukum Perusahaan Kekayaan Intelektual Investasi Properti Telekomunikasi Notaris" />
+<link rel="canonical" href="https://amaradvokat.com">
+<meta property="og:image" content="{{ asset('storage/company'.'/'.$web_config['web_logo']) }}" />
+<meta property="og:title" content="Amar Advokat Company Profile Firma Hukum" />
+<meta property="og:url" content="https://amaradvokat.com/">
+<meta property="og:description" content="amar advokat Hukum Perusahaan - Hak Kekayaan Intelektual - Investasi - Properti - Pertambangan & Energi - Telekomunikasi - Dokumentasi Notaris - Perbankan & Keuangan - Asuransi - Ketenagakerjaan - Hukum Keluarga dan Perkawinan - Hukum Perdata & Pidana - Pengacara Retainer">
+
+<meta property="twitter:card" content="{{ asset('storage/company'.'/'.$web_config['web_logo']) }}" />
+<meta property="twitter:title" content="Amar Advokat Company Profile Firma Hukum" />
+<meta property="twitter:url" content="https://amaradvokat.com/">
+<meta property="twitter:description" content="Hukum Perusahaan - Hak Kekayaan Intelektual - Investasi - Properti - Pertambangan & Energi - Telekomunikasi - Dokumentasi Notaris - Perbankan & Keuangan - Asuransi - Ketenagakerjaan - Hukum Keluarga dan Perkawinan - Hukum Perdata & Pidana - Pengacara Retainer">

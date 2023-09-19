06b0895a6d1414c90492e160cf3c97a76f0369f5
dhaydt <1992dayat@gmail.com>
2023-09-19 16:13:21 +0700

    meta seo

--- resources/views/Home/partials/meta.blade.php
+++ resources/views/Home/partials/meta.blade.php
@@ -1,4 +1,4 @@
-{{-- <meta name="title" content="Amar Advokat || Website Hukum Advokat" />
+<meta name="title" content="Amar Advokat || Website Hukum Advokat" />
 <meta name="description" content="Amar Advokat, Hukum Perusahaan,  Hak Kekayaan Intelektual, Investasi, Properti, Pertambangan & Energi, Dokumentasi Notaris, Perbankan & Keuangan, Asuransi, Ketenagakerjaan, Hukum Keluarga dan Perkawinan, Hukum Perdata & Pidana, Pengacara Retainer" />
 <meta name="keyword" content="amar advokat amaradvokat Hukum Perusahaan Kekayaan Intelektual Investasi Properti Telekomunikasi Notaris" />
 <link rel="canonical" href="https://amaradvokat.com">
@@ -10,4 +10,4 @@
 <meta property="twitter:card" content="{{ asset('storage/company'.'/'.$web_config['web_logo']) }}" />
 <meta property="twitter:title" content="Amar Advokat Company Profile Firma Hukum" />
 <meta property="twitter:url" content="https://amaradvokat.com/">
-<meta property="twitter:description" content="Hukum Perusahaan - Hak Kekayaan Intelektual - Investasi - Properti - Pertambangan & Energi - Telekomunikasi - Dokumentasi Notaris - Perbankan & Keuangan - Asuransi - Ketenagakerjaan - Hukum Keluarga dan Perkawinan - Hukum Perdata & Pidana - Pengacara Retainer"> --}}
+<meta property="twitter:description" content="Hukum Perusahaan - Hak Kekayaan Intelektual - Investasi - Properti - Pertambangan & Energi - Telekomunikasi - Dokumentasi Notaris - Perbankan & Keuangan - Asuransi - Ketenagakerjaan - Hukum Keluarga dan Perkawinan - Hukum Perdata & Pidana - Pengacara Retainer">
